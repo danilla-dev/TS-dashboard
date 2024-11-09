cac40760e900f9e7fde19be5ffd44199ae1d458c
Danilla <122106121+danilla-dev@users.noreply.github.com>
2024-11-10 00:35:43 +0100

    Added a functionality of cashe and save in session storage data fetchin in first time by hook.

--- src/hooks/useEventsData.tsx
+++ src/hooks/useEventsData.tsx
@@ -1,40 +1,68 @@
 import { useState, useEffect } from 'react'
 import { faker } from '@faker-js/faker'
 import { Event } from '../types/events.types'
+import { useQuery } from 'react-query'
 
+const generateEventsData = (count = 15): Event[] => {
+	return Array.from({ length: count }, () => {
+		const startDate = new Date('2022-01-01T00:00:00.000Z')
+		const endDate = new Date('2025-12-31T23:59:59.999Z')
+		const eventDate = faker.date.between({ from: startDate, to: endDate }).toISOString().split('T')[0]
+		const today = new Date().toISOString().split('T')[0]
+
+		let status: 'Upcoming' | 'Ongoing' | 'Completed'
+		if (eventDate > today) {
+			status = 'Upcoming'
+		} else if (eventDate < today) {
+			status = 'Completed'
+		} else {
+			status = 'Ongoing'
+		}
+
+		return {
+			id: faker.string.uuid(),
+			eventName: faker.company.name(),
+			date: eventDate,
+			participants: faker.number.int({ min: 50, max: 500 }),
+			status: status,
+		}
+	})
+}
 export const useEventsData = (): Event[] | null => {
-	const [eventsData, setEventsData] = useState<Event[] | null>(null)
+	const queryKey = 'eventsData'
 
-	const generateEventsData = (count = 15): Event[] => {
-		return Array.from({ length: count }, () => {
-			const startDate = new Date('2022-01-01T00:00:00.000Z')
-			const endDate = new Date('2025-12-31T23:59:59.999Z')
-			const eventDate = faker.date.between({ from: startDate, to: endDate }).toISOString().split('T')[0]
-			const today = new Date().toISOString().split('T')[0]
-
-			let status: 'Upcoming' | 'Ongoing' | 'Completed'
-			if (eventDate > today) {
-				status = 'Upcoming'
-			} else if (eventDate < today) {
-				status = 'Completed'
-			} else {
-				status = 'Ongoing'
-			}
-
-			return {
-				id: faker.string.uuid(),
-				eventName: faker.company.name(),
-				date: eventDate,
-				participants: faker.number.int({ min: 50, max: 500 }),
-				status: status,
-			}
-		})
+	const loadDataFromStorage = (): Event[] | null => {
+		const storedData = sessionStorage.getItem(queryKey)
+		return storedData ? JSON.parse(storedData) : null
 	}
 
-	useEffect(() => {
-		const data = generateEventsData()
-		setEventsData(data)
-	}, [])
+	const saveDataToStorage = (data: Event[]) => {
+		sessionStorage.setItem(queryKey, JSON.stringify(data))
+	}
 
-	return eventsData
+	const { data, isLoading, isError } = useQuery<Event[]>(
+		[queryKey],
+		() => {
+			const dataFromStorage = loadDataFromStorage()
+			if (dataFromStorage) {
+				return dataFromStorage
+			} else {
+				const generatedData = generateEventsData(15)
+				saveDataToStorage(generatedData)
+				return generatedData
+			}
+		},
+		{
+			enabled: true,
+			refetchOnWindowFocus: false,
+			refetchOnReconnect: false,
+			staleTime: 1000 * 60 * 5,
+			cacheTime: 1000 * 60 * 60,
+		}
+	)
+
+	if (isLoading) console.log('Skeleton in future')
+	if (isError) console.log('Error page in future')
+
+	return data || null
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider, createSystem } from '@chakra-ui/react'
 import { system } from '../theme.js'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<ChakraProvider value={system}>
-			<Router>
-				<App />
-			</Router>
-		</ChakraProvider>
+		<QueryClientProvider client={queryClient}>
+			<ChakraProvider value={system}>
+				<Router>
+					<App />
+				</Router>
+			</ChakraProvider>
+		</QueryClientProvider>
 	</StrictMode>
 )
